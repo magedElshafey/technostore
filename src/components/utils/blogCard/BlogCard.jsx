d8a8fb09cd3cd41cc63bf7f73912fc0b8afd95de
magedElshafey <magedelshafey98@gmail.com>
2023-11-13 07:18:20 +0200

    add read more button to blog card

--- src/components/utils/blogCard/BlogCard.jsx
+++ src/components/utils/blogCard/BlogCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./blogcard.module.css";
+import { AiOutlineArrowRight } from "react-icons/ai";
 const BlogCard = ({ data }) => {
   return (
     <div className={style.card}>
@@ -8,6 +9,11 @@ const BlogCard = ({ data }) => {
       </div>
       <p className={`m-0 p-0 my-3 ${style.title}`}>{data.title}</p>
       <p className={`m-0 p-0 ${style.desc}`}>{data.desc}</p>
+
+      <button className={style.btn}>
+        <span> read more</span>
+        <AiOutlineArrowRight />
+      </button>
     </div>
   );
 };
